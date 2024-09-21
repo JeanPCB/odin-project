05673f1c56f76effa9c7437c1dd4d73813f8171f
jeanpcb <jeanp.cebinel01@gmail.com>
2024-09-21 18:20:58 -0300

    init odin project ruby

---
+++ ruby-learn/data-types/data_types1.rb
@@ -0,0 +1,23 @@
+### DATA TYPES ###
+## 4 basic datatypes -> Number, String, Symbol, Boolean
+
+# Number
+num1 = 2   # integer
+num2 = 5.5 # float
+
+# String
+my_string = "string"
+
+# Symbol
+my_symbol = :its_my_symbol
+
+# Boolean
+is_true = true   # true
+is_false = false # false
+
+puts "Number 1 = #{num1}"
+puts "Number 2 = #{num2}"
+puts "My string = #{my_string}"
+puts "My simbol = #{my_symbol}"
+puts "True = #{is_true}"
+puts "False = #{is_false}"
